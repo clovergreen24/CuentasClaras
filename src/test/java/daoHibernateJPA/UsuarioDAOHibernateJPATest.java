0f307b3e6c46e18f20dbbf715c08302817c5afd9
Manaaa22 <keimmariana@gmail.com>
2023-10-25 20:38:06 -0300

    TestUsuario

--- src/test/java/daoHibernateJPA/UsuarioDAOHibernateJPATest.java
+++ src/test/java/daoHibernateJPA/UsuarioDAOHibernateJPATest.java
@@ -3,6 +3,7 @@ package daoHibernateJPA;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import componentes.Gasto;
@@ -10,8 +11,11 @@ import componentes.Grupo;
 import componentes.Pago;
 import componentes.Saldo;
 import componentes.Usuario;
+import dao.impl.GenericDAOHibernateJPA;
 
 public class UsuarioDAOHibernateJPATest {
+	
+	private GenericDAOHibernateJPA<Usuario> usuario;    //?????
 	
 @Test
 public void testCrearUsuario() {
@@ -27,7 +31,12 @@ public void testCrearUsuario() {
 	List<Usuario> amigos2 = new ArrayList<>();
 	List<Saldo> saldos2 = new ArrayList<>();
 	
-	final Usuario usr1= new Usuario(1L, "clovergreen", "Camilo", "contraseña1", "cobeagac@gmail.com", "ruta/foto1.jpg", grupos1, pagos1, gastos1, amigos1, saldos1);
-	final Usuario usr2= new Usuario(2L, "Mana", "Mariana", "contraseña2", "keimmariana@gmail.com", "ruta/foto2.jpg", grupos2, pagos2, gastos2, amigos2, saldos2);
+	final Usuario usr1= new Usuario(1L, "clovergreen", "Camilo", "contrasenia1", "cobeagac@gmail.com", "ruta/foto1.jpg", grupos1, pagos1, gastos1, amigos1, saldos1);
+	final Usuario usr2= new Usuario(2L, "Mana", "Mariana", "contrasenia2", "keimmariana@gmail.com", "ruta/foto2.jpg", grupos2, pagos2, gastos2, amigos2, saldos2);
+	//Assertions.assertEquals(0, usr1.getAmigos().size());
+	
+	usuario = new GenericDAOHiernateJPA(); 
+	Assertions.assertEquals(2, usuario.recuperarTodos("nombre").size());
+
 }
 }
